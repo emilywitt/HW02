85958295e8dce76adcba64e2ec28e033c06c2e9d
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-05 19:08:21 -0700

    EW ch 3 HW 04

--- HW02_ex03_04.py
+++ HW02_ex03_04.py
@@ -18,20 +18,60 @@
 # 1. Type this example into this script and test it.
 # 2. Modify do_twice so that it takes two arguments, a function object and a value,
 #  and calls the function twice, passing the value as an argument.
+#def print_spam(x):									
+#    print x
+#def do_twice(f,b):
+#    f(b)
+#    f(b)
+#
+#do_twice(print_spam,'emily')
 # 3. Write a more general version of print_spam, called print_twice, that takes a 
-# string as a parameter and prints it twice.
+# string as a parameter and prints it twice. 
+#def print_twice(s)
+#    print s
+#    print s
 # 4. Use the modified version of do_twice to call print_twice twice, passing 'spam'
 #  as an argument.
+#do_twice(print_twice, 'spam')
 # 5. Define a new function called do_four that takes a function object and a value 
 # and calls the function four times, passing the value as a parameter. There 
 # should be only two statements in the body of this function, not four.
+#def do_four(f,b):
+#	f(b)
+#	f(b)
+
+#do_four(print_twice,'emily')
 ################################################################################
 # Write your functions below:
 # Body
 
+def do_twice(f):
+    f()
+    f()
 
+def print_spam(x):									
+    print x
 
+def print_twice(s):
+    print s
+    print s
 
+print_twice('emily')
+
+def do_twice(f,b):
+    f(b)
+    f(b)
+
+do_twice(print_spam,'emily')
+
+do_twice(print_twice, 'spam')
+
+def do_four(f,b):
+	f(b)
+	f(b)
+
+do_four(print_twice,'emily')
+    
 
 
 
